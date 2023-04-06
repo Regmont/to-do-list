7a9aeca159f9b2fc894f2ec411f356275320a47f
Дунин Михаил <karmodziregmont@gmail.com>
2023-04-06 20:54:31 +0300

    more logic

--- to-do_list/to-do_list.cpp
+++ to-do_list/to-do_list.cpp
@@ -3,16 +3,113 @@
 #include <windows.h>
 using namespace std;
 
-struct doing
+int months[12]{ 31,28,31,30,31,30,31,31,30,31,30,31 };
+
+struct Date
+{
+	int day;
+	int month;
+	int year;
+
+	int getAllDays()
+	{
+		int days = day;
+
+		for (int i = month - 2; i >= 0; i--) {
+			days += months[i];
+		}
+		days += 365 * (year - 1);
+
+		if (year % 4 == 0)
+			days += year / 4 - 1;
+		else
+			days += year / 4;
+
+		return days;
+	}
+
+	string info()
+	{
+		string str;
+		if (day < 10)
+			str.append("0");
+		str.append(std::to_string(day));
+
+		str.append(".");
+
+		if (month < 10)
+			str.append("0");
+		str.append(std::to_string(month));
+
+		str.append(".");
+
+		str.append(std::to_string(year));
+
+		return str;
+	}
+};
+
+struct Time
+{
+	int hour;
+	int minute;
+
+	int getAllTime()
+	{
+		int minutes = minute;
+
+		minutes += (hour - 1) * 60;
+
+		return minutes;
+	}
+
+	string info()
+	{
+		string str;
+		if (hour < 10)
+			str.append("0");
+		str.append(std::to_string(hour));
+
+		str.append(":");
+
+		if (minute < 10)
+			str.append("0");
+		str.append(std::to_string(minute));
+
+		return str;
+	}
+};
+
+struct Task
 {
 	string name;
 	int priority;
 	string description;
-	string creat_date;
-	float compl_time;
+	Date create_date;
+	Time compl_time;
+
+	string info() {
+		string str = "";
+		str.append("Название: ");
+		str.append(name);
+		str.append("\n");
+		str.append("Приоритет: ");
+		str.append(std::to_string(priority));
+		str.append("\n");
+		str.append("Описание: ");
+		str.append(description);
+		str.append("\n");
+		str.append("Дата: ");
+		str.append(create_date.info());
+		str.append("\n");
+		str.append("Время выполнения: ");
+		str.append((compl_time.info()));
+		str.append("\n");
+		return str;
+	}
 };
 
-void create_a_doing(doing& x)
+void createTask(Task& x)
 {
 	cout << "Введите название дела: ";
 	cin >> x.name;
@@ -20,19 +117,79 @@ void create_a_doing(doing& x)
 	cin >> x.priority;
 	cout << "Введите описание дела: ";
 	cin >> x.description;
-	cout << "Введите дату появления дела: ";
-	cin >> x.creat_date;
-	cout << "Введите время на исполнение дела (в часах): ";
-	cin >> x.compl_time;
+	cout << "Введите дату появления дела (день, месяц, год): ";
+	cin >> x.create_date.day >> x.create_date.month >> x.create_date.year;
+	cout << "Введите время исполнения дела (час, минуты): ";
+	cin >> x.compl_time.hour >> x.compl_time.minute;
+	cout << endl;
 }
 
-void print_a_doing(doing x)
+void addTask(Task*& array, int& size, Task new_task)
 {
-	cout << "Название дела: " << x.name << endl;
-	cout << "Приоритет дела: " << x.priority << endl;
-	cout << "Описание дела: " << x.description << endl;
-	cout << "Дата появления дела: " << x.creat_date << endl;
-	cout << "Время на исполнение дела: " << x.compl_time << endl;
+	Task* temp = new Task[size + 1];
+
+	for (int i = 0; i < size; i++)
+		temp[i] = array[i];
+
+	temp[size] = new_task;
+
+	delete[] array;
+	array = temp;
+	size++;
+}
+
+string reverseString(string s)
+{
+	string res;
+
+	for (int i = s.length() - 1; i >= 0; i--)
+		res += s[i];
+
+	return res;
+}
+
+void bubbleSort(Task* arr, int size)
+{
+	int i, j;
+
+	for (i = 0; i < size - 1; i++)
+		for (j = 0; j < size - i - 1; j++)
+			if (arr[j].create_date.getAllDays() > arr[j + 1].create_date.getAllDays())
+				swap(arr[j], arr[j + 1]);
+}
+
+int findTask(Task* array, int size, string del_name)
+{
+	for (int i = 0; i < size; i++)
+		if (array[i].name.compare(del_name) == 0)
+			return i;
+
+	return -1;
+}
+
+void deleteTask(Task*& array, int& size, int chosen_task)
+{
+	Task* temp = new Task[size - 1];
+
+	for (int i = 0; i < chosen_task; i++)
+		temp[i] = array[i];
+
+	for (int i = chosen_task + 1; i < size; i++)
+		temp[i - 1] = array[i];
+
+	delete[] array;
+	array = temp;
+	size--;
+}
+
+void printArray(Task* arr, int size)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		cout << arr[i].info() << "\n\n";
+
+	cout << endl;
 }
 
 int main()
@@ -40,9 +197,35 @@ int main()
 	setlocale(LC_ALL, "ru");
 	SetConsoleCP(1251);
 
-	doing eat;
-	create_a_doing(eat);
-	print_a_doing(eat);
+	Task* array = new Task[0];
+	int size = 0;
+	Task new_task;
+	int amount;
+
+	cout << "Введите, сколько дел хотите добавить в список: ";
+	cin >> amount;
+
+	for (int i = 0; i < amount; i++)
+	{
+		createTask(new_task);
+		addTask(array, size, new_task);
+	}
+
+	printArray(array, size);
+
+	string del_name;
+	int del_index;
+
+	cout << "Введите название дела для удаления\n";
+	cin >> del_name;
+
+	del_index = findTask(array, size, del_name);
+	if (del_index != -1)
+		deleteTask(array, size, del_index);
+	else
+		cout << "Дело не найдено\n";
+
+	printArray(array, size);
 }
 
 /*
@@ -50,14 +233,6 @@ int main()
 Создайте приложение «Список дел».
 
 Приложение должно позволять:
-
-■ Добавление дел. У дела есть:
-• название;
-• приоритет;
-• описание;
-• дата, время исполнения.
-
-■ Удаление дел.
 
 ■ Редактирование дел.
 
@@ -77,3 +252,4 @@ int main()
 • по дате и времени исполнения.
 
 */
+
